0f606b02a1eff1da1713319613b91e0fd5848ddc
JasGiigli <overview.jjj@gmail.com>
2023-12-27 12:14:20 -0800

    first commit

---
+++ src/app/components/Footer/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-900 text-white py-4">
+      <div className="container mx-auto px-4 flex items-center justify-center">
+        <p className="text-center text-sm">&copy; 2023 JasGiigli. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
